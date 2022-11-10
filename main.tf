1736371d5eb3c862d9844cf9f3b47bbc691bdee8
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-11-10 14:02:21 +0300

    vpc zones

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 //Provider to use aws,azure,gcp,heroku etc can be multiple as terraform is cloud agnostic
 provider "aws" {
   region = "us-east-1"
-
 }
 
 variable "instance_type"{
@@ -10,7 +9,7 @@ variable "instance_type"{
 
 //Inline with code
 locals {
-  project_name = "Alvin"
+  project_name = "Alvin-learning"
 }
 
 //Resource to be created
@@ -29,4 +28,28 @@ resource "aws_instance" "my_server" {
 //Use terraform refresh to view the output or terraform output
 output "instance_ip_addr" {
   value = aws_instance.my_server.public_ip
+}
+
+//Models form terraform registry
+//Terrafrom init to install the module
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  //Availability zone
+  //Subnets can currently only be created in the following availability 
+  //availability zones: us-east-1a, us-east-1b, us-east-1c, us-east-1d, us-east-1e, us-east-1f
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1d"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
