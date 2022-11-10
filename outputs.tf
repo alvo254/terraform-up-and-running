24d0beb8439f38c05dfc83b9e6d33b6922eef651
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-11-10 17:36:50 +0300

    refactored into folders

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-//Has to have the resource name 
-//This allows us to see the values
-//Use terraform refresh to view the output or terraform output
-output "instance_ip_addr" {
-  value = aws_instance.my_server.public_ip
-}
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-//This file is used to store variables
-instance_type="t2.micro"
