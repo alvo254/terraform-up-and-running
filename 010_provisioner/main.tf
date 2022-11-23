18a74e083b86c1b413725f95597b152c4721a634
alvo254 <41018786+alvo254@users.noreply.github.com>
2022-11-24 00:50:34 +0300

    some changes

--- 010_provisioner/main.tf
+++ 010_provisioner/main.tf
@@ -22,6 +22,7 @@ resource "aws_instance" "my_server" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.deployer.key_name}"
+  vpc_security_group_ids = [aws_security_group.sg_my_server.id]
 
   tags = {
     //String interpolation in terraform 
@@ -36,4 +37,37 @@ resource "aws_key_pair" "deployer" {
 
 output "public_ip" {
   value = aws_instance.my_server
+}
+
+//data sources allow us to refernce external resources
+data "aws_vpc" "main"{
+    id = "vpc-0df4beebc1a0823b4"
+}
+
+resource "aws_security_group" "sg_my_server" {
+  name        = "sg_my_server"
+  description = "mySever security group"
+  vpc_id      = data.aws_vpc.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    //The /32 means use a single ip
+    ipv6_cidr_blocks = ["105.163.30.119/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
