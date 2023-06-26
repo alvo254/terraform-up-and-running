9c12850b23e96c7de54230a6c4181b65f8d44a2e
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-06-26 21:04:22 +0300

    fixed resources in aws

--- 020_null_resources/main.tf
+++ 020_null_resources/main.tf
@@ -14,6 +14,33 @@ terraform {
   }
 }
 
+provider "aws" {
+    region = "us-east-1"
+}
 
+locals {
+	project_name = "null_resources"
+}
+
+
+//Resource to be created
+resource "aws_instance" "my_server" {
+  ami = "ami-08c40ec9ead489470"
+  instance_type = "t2.micro"
+
+  tags = {
+    //String interpolation in terraform 
+    Name = "Myserver-${local.project_name}"
+  }
+}
+
+resource "null_resource" "status" {
+  provisioner "local-exec" {
+    command = "awd ec2 wait instance-status-ok --instance-ids ${aws_instance.my_server.id}"
+  }
+  depends_on = [
+    aws_instance.my_server
+  ]
+}
 
 
