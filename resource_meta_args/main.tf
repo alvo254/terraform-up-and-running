e428e95f2595d3b0c9d5a6095d3760760cece8e2
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-07-22 23:03:45 +0300

    Update main.tf

--- resource_meta_args/main.tf
+++ resource_meta_args/main.tf
@@ -38,3 +38,9 @@ output "public_ip" {
   description = "Get instance public IP"
 
 }
+
+
+resource "aws_db_instance" "example" { identifier_prefix   = "terraform-up-and-running" engine              = "mysql" allocated_storage   = 10 instance_class      = "db.t2.micro" skip_final_snapshot = true username = var.db_username password = var.db_password }
+
+
+
