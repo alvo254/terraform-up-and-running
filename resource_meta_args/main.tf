f74ff5b14bc49b9d32f26a8f2a1ad11dd27771be
Alvin <alvinkhamron@gmail.com>
2023-12-13 13:58:19 +0300

    update

--- resource_meta_args/main.tf
+++ resource_meta_args/main.tf
@@ -40,7 +40,6 @@ output "public_ip" {
 }
 
 
-resource "aws_db_instance" "example" { identifier_prefix   = "terraform-up-and-running" engine              = "mysql" allocated_storage   = 10 instance_class      = "db.t2.micro" skip_final_snapshot = true username = var.db_username password = var.db_password }
 
 
 
